1ad81177ef8ae7147cc60fbfaeb0f5e22be25d51
monks-commits <monks@mail.ru>
2026-02-10 18:35:46 +0200

    Delete backoffice directory

--- backoffice/supabaseClient.js
+++
@@ -1,9 +0,0 @@
-// backoffice/supabaseClient.js
-// Оставляем файл чтобы не ломать структуру репо.
-// В backoffice V1 мы НЕ используем Supabase и НЕ требуем anon_key.
-(function(){
-  window.BO_SUPABASE = {
-    enabled: false,
-    reason: 'Backoffice V1 працює локально (без авторизації та без Supabase).'
-  };
-})();
